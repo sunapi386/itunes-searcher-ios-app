06c477f9ae9f7eff44009f06aed8e25e1d8c0f13
Jason Sun <j_sun@apple.com>
2016-10-25 14:05:41 -0700

    fake row contents, add a playground

--- iTunesSearch/MusicTableViewController.swift
+++ iTunesSearch/MusicTableViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class MusicTableViewController: UITableViewController {
 
+    var content = ["Whatever", "Stranger Things", "24k Magic",
+                   "Set Theory", "Friends", "Dexter", "Breaking Bad", "Lakers",
+                   "2016 Election", "The Apprentice"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,25 +31,20 @@ class MusicTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return content.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath)
 
         // Configure the cell...
+        let row = indexPath.row
+        let object = content[row]
+        cell.textLabel?.text = object
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
---
+++ iTunesSearch/MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import PlaygroundSupport // so it runs continuously
+// async support
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+// Just get a URL
+let baseURL = "https://itunes.apple.com/search?parameterkeyvalue"
+if var components = URLComponents(string: baseURL) {
+    components.queryItems?.append(URLQueryItem(name: "term", value: "lady gaga"))
+    components.queryItems?.append(URLQueryItem(name: "term", value: "lady gaga"))
+    components.url
+    let session = URLSession.shared
+    if let url = components.url {
+        let task = session.dataTask(with: url, completionHandler: { (data: Data?, resp: URLResponse?, err: Error?) in
+            debugPrint(data)
+            debugPrint(resp)
+            debugPrint(err)
+        })
+        task.resume()
+    }
+}
+
